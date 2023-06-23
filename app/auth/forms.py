92cb6142acde90388cdabddcc43123f0604c44d3
David Ekunno <matramere@gmail.com>
2023-06-23 10:48:57 -0500

    updated app temps/added routes

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, EqualTo
+
+class RegisterForm(FlaskForm):
+    pass
+
+class LoginForm(FlaskForm):
+    pass
