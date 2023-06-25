b26b4dc840548b6fb0027e3bea086b96ac55c4f0
Jposeph99 <joseph.ahmed65@yahoo.com>
2023-06-25 15:35:25 -0600

    files added

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,30 @@ from flask import Flask
 from config import Config
 
 from .auth.routes import auth
-from .ig.routes import ig
+# from .ig.routes import ig
 from .models import db, User
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_moment import Moment
-from .api.routes import api
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+login = LoginManager()
+
+# @login.user_loader
+# def load_user(user_id):
+#     return Customer.query.get(customer_id)
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+login.init_app(app)
+
+login.login_view = 'auth.login'
+
+# moment = Moment(app)
+
+app.register_blueprint(auth)
+
+from . import routes
+from . import models
--- app/models.py
+++ app/models.py
@@ -3,3 +3,4 @@ from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 
+db = SQLAlchemy()
