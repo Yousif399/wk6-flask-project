78a58748e9ffc10512c59f93e472b3f427ec9653
qwertyNippon <orlandohenry100@gmail.com>
2023-06-25 19:28:35 -0400

    qwerty

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,2 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo
+
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,5 @@ from app import app
 
 from flask import render_template
 from .models import User
-from flask_login import current_user
 
 
